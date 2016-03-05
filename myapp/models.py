cf9e1654f36c58b864a8893105e1ded717326914
Kaleb Davis <kalebdavisgithub@gmail.com>
2016-03-05 13:58:29 -0500

    add static folder and getter to model

--- myapp/models.py
+++ myapp/models.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import ics
 
 class Event(models.Model):
     name = models.CharField(max_length=140)
@@ -11,3 +12,6 @@ class Calendar(models.Model):
     event = models.ForeignKey(Event)
     username = models.CharField(max_length=100)
     contents = models.TextField()
+
+    def getCalendar(self):
+        return ics.Calendar(contents.decode('iso-8859-1'))
