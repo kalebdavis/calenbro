4ee9154c26fc4839dce2f41039c3906444b98e72
Kaleb Davis <kalebdavisgithub@gmail.com>
2016-03-08 19:45:20 -0500

    update database settings

--- Calenbro/settings.py
+++ Calenbro/settings.py
@@ -118,6 +118,10 @@ USE_L10N = True
 
 USE_TZ = True
 
+# Update database configuration with $DATABASE_URL
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
